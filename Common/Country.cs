9e321b72708554c8951dec80371fe7155ffbebda
Alex Harris <a.harris31@outlook.com>
2022-05-23 22:41:23 +0100

    Added alternative name to help searching and moved search to top to improve experience on mobile

--- Common/Country.cs
+++ Common/Country.cs
@@ -5,4 +5,5 @@ public class Country
     public string Code { get; set; } = null!;
     public string Name { get; set; } = null!;
     public string Continent { get; set; } = null!;
+    public string? AlternativeName { get; set; }
 }
