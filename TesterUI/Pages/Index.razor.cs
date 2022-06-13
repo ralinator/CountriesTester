da23ab7bae016acdba191ec44ea3cadcca283216
Alex Harris <a.harris31@outlook.com>
2022-06-13 23:29:18 +0100

    Improve country dropdown

--- TesterUI/Pages/Index.razor.cs
+++ TesterUI/Pages/Index.razor.cs
@@ -1,5 +1,7 @@
+using Common;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using Radzen;
 using System.Text.Json;
 using TesterUI.Services;
 using TesterUI.State;
@@ -13,16 +15,32 @@ public partial class Index
 
     private AnswerSubmission _answerSubmission = new();
 
+    private IEnumerable<Country>? _countriesList;
+
     protected override async Task OnInitializedAsync()
     {
         await GameStateService.InitializeAsync();
         await base.OnInitializedAsync();
     }
 
+    private void FilterCountriesList(LoadDataArgs args)
+    {
+        _countriesList = GameStateService.GameData.Countries.AsEnumerable();
+        if (!string.IsNullOrEmpty(args.Filter))
+        {
+            _countriesList = _countriesList.Where(q =>
+            q.Name.ToLower().Contains(args.Filter.ToLower()) ||
+            (q.AlternativeName == null ? false : q.AlternativeName.ToLower().Contains(args.Filter.ToLower()))
+            );
+        }
+        _countriesList = _countriesList.OrderBy(q => q.Name).ToList();
+    }
+
     private async Task OnAnswerSubmission()
     {
         await GameStateService.SubmitAnswer(_answerSubmission);
         _answerSubmission = new();
+        FilterCountriesList(new());
     }
     
     private async Task ResumeGame(string id)
